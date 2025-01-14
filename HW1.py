5dde9e5734808361e4e39529f30a3d20beb1f8d2
Richard Glatter <rglatter.ieu2023@student.ie.edu>
2025-01-14 17:11:45 +0100

    HW1

---
+++ HW1.py
@@ -0,0 +1,14 @@
+print("this is the first homework assignment")
+print(10+20)
+Variable1 = 100
+Variable2 = 20
+print(Variable1+Variable2)
+
+Variable3 = "Hello"
+Variable4 = "World"
+print(Variable3+Variable4)
+print("I was not sure what else to do")
+print("Test")
+
+print("New code after commiting and pushing")
+print("to see if it works")
